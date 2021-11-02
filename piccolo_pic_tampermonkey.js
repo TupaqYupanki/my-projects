893ea9b6f5144c7d27d5f8b26df3e8f9cafcf00a
TupaqYupanki <rustarkmad@gmail.com>
2021-11-03 00:40:02 +0300

    1

--- piccolo_pic_tampermonkey.js
+++ piccolo_pic_tampermonkey.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://piccolo-detki.ru/kolyaski-spb/kupit-kolyasku/tutis-zippy/tutis-detskaya-kolyaska-3-v-1-uno-plus-2019-risso-142/
+// @match        https://piccolo-detki.ru/*
 // @icon         https://www.google.com/s2/favicons?domain=piccolo-detki.ru
 // @grant        none
 // ==/UserScript==
