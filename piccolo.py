21a5a6f038f336786abb56b26fbdacb8d2fa4398
TupaqYupanki <rustarkmad@gmail.com>
2021-11-10 17:02:59 +0300

    1

--- piccolo.py
+++ piccolo.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 
 #Читаем книгу
-wb = load_workbook('3.11.2021 (1).xlsx')
+wb = load_workbook('Книга1 (2).xlsx')
 
 #делаем активной 2ую страницу
 wb.active = 0
@@ -20,12 +20,14 @@ for row in ws.iter_rows(min_row=1, min_col=3, max_col=3, max_row=200, values_onl
           print(piccolo)
           url = requests.get(piccolo)
           if '200' in str(url):
-              soup = BeautifulSoup(url.content)
+              soup = BeautifulSoup(url.content, features="html.parser")
               mydivs = soup.find_all("span", class_= "ty-price-num")
               if mydivs != []:
                   ws.cell(row=y, column=20).value = mydivs[0].text
               else: ws.cell(row=y, column=20).value = 'price not found'
-          else: print('url unreachable '+piccolo)
+          else:
+              print('url unreachable '+piccolo)
+              ws.cell(row=y, column=20).value = 'url unreachable'
     y=y+1
 wb.save('test.xlsx')
 
@@ -38,12 +40,14 @@ for row in ws.iter_rows(min_row=1, min_col=5, max_col=5, max_row=200, values_onl
             print(lapsi)
             url = requests.get(lapsi, headers={'User-Agent':'Test'})
             if '200' in str(url):
-                soup = BeautifulSoup(url.content)
+                soup = BeautifulSoup(url.content, features="html.parser")
                 mydivs = soup.find_all("div", class_= "price")[0]
                 if mydivs != []:
                     ws.cell(row=z, column=21).value = mydivs.contents[0]
                 else: ws.cell(row=z, column=21).value = 'price not found'
-            else: print('url unreachable '+lapsi)
+            else:
+                print('url unreachable '+lapsi)
+                ws.cell(row=z, column=21).value = 'url unreachable'
     z=z+1
 
 wb.save('test.xlsx')
@@ -56,12 +60,14 @@ for row in ws.iter_rows(min_row=1, min_col=9, max_col=9, max_row=200, values_onl
             print(olant)
             url = requests.get(olant)
             if '200' in str(url):
-                soup = BeautifulSoup(url.content)
+                soup = BeautifulSoup(url.content, features="html.parser")
                 mydivs = soup.find("div", class_= "card-price__current")
                 if mydivs != []:
                     ws.cell(row=t, column=22).value = str(mydivs.text)[:-5]
                 else: ws.cell(row=t, column=22).value = 'price not found'
-            else: print('url unreachable '+olant)
+            else:
+                print('url unreachable '+olant)
+                ws.cell(row=t, column=22).value = 'url unreachable'
     t=t+1
 
 wb.save('test.xlsx')
@@ -76,12 +82,14 @@ for row in ws.iter_rows(min_row=1, min_col=13, max_col=13, max_row=200, values_o
             akusherId = akusher.split('/')[4].split('-')[0]
             url = requests.get(akusher)
             if '200' in str(url):
-                soup = BeautifulSoup(url.content)
+                soup = BeautifulSoup(url.content, features="html.parser")
                 mydivs = soup.find("span", id="tovar_price_"+str(akusherId))
                 if mydivs != []:
                     ws.cell(row=a, column=23).value = str(mydivs.text)
                 else: ws.cell(row=a, column=23).value = 'price not found'
-            else: print('url unreachable '+akusher)
+            else:
+                print('url unreachable '+akusher)
+                ws.cell(row=a, column=23).value = 'url unreachable'
     a=a+1
 
 wb.save('test.xlsx')
