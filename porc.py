f0622a402433746496e17e3b7b9f2cf1999b33b7
TupaqYupanki <rustarkmad@gmail.com>
2022-01-21 19:49:28 +0300

    ragna

--- porc.py
+++ porc.py
@@ -6,42 +6,43 @@ import random
 import win32api
 import win32con
 #1360 768
-i = 0
 while keyboard.is_pressed('q') == False:
-    z = pyautogui.locateCenterOnScreen('egg3.png', region = (0,0,1400,800), confidence=0.8)
-    time.sleep(0.03)
-    a = pyautogui.locateCenterOnScreen('egg4.png', region = (0,0,1400,800), confidence=0.8)
-    time.sleep(0.03)
-    b = pyautogui.locateCenterOnScreen('egg5.png', region = (0,0,1400,800), confidence=0.8)
+    z = pyautogui.locateCenterOnScreen('porc1.png', region = (0,0,1400,800), confidence=0.65)
+    a = pyautogui.locateCenterOnScreen('porc2.png', region = (0,0,1400,800), confidence=0.65)
+    b = pyautogui.locateCenterOnScreen('porc3.png', region = (0,0,1400,800), confidence=0.65)
+    c = pyautogui.locateCenterOnScreen('porc4.png', region = (0,0,1400,800), confidence=0.65)
     # if pyautogui.locateOnScreen('egggg.png', confidence=0.8) != None:
     if keyboard.is_pressed('w'):
         time.sleep(5)
-    elif z != None and i < 5:
+    elif z != None:
         x, y = z
-        pyautogui.moveTo(x, y, 0.13)
+        # pyautogui.press('f2')
         pyautogui.click(x, y)
         pyautogui.moveTo((x+(random.uniform(0, 30))), (y+(random.uniform(0, 30))), 0.13)
         print('found z')
-        i = i+1
-        time.sleep(random.uniform(1.8, 2.0))
-    elif a != None and i < 5:
+        time.sleep(2.3)
+    elif a != None:
         x, y = a
-        pyautogui.moveTo(x, y, 0.13)
+        # pyautogui.press('f2')
         pyautogui.click(x, y)
         pyautogui.moveTo((x+(random.uniform(0, 30))), (y+(random.uniform(0, 30))), 0.13)
         print('found a')
-        i = i+1
-        time.sleep(random.uniform(1.8, 2.0))
-    elif b != None and i < 5:
+        time.sleep(2.3)
+    elif b != None:
         x, y = b
-        pyautogui.moveTo(x, y, 0.13)
+        # pyautogui.press('f2')
         pyautogui.click(x, y)
         pyautogui.moveTo((x+(random.uniform(0, 30))), (y+(random.uniform(0, 30))), 0.13)
         print('found b')
-        i = i+1
-        time.sleep(random.uniform(1.8, 2.0))
+        time.sleep(2.3)
+    elif c != None:
+        x, y = c
+        # pyautogui.press('f2')
+        pyautogui.click(x, y)
+        pyautogui.moveTo((x+(random.uniform(0, 30))), (y+(random.uniform(0, 30))), 0.13)
+        print('found b')
+        time.sleep(2.3)
     else:
-        i = 0
         print("I am unable to see it")
         pyautogui.press('f1')
         time.sleep(0.1)
